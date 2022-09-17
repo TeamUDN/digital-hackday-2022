c3a0e0d4762f5483160a19a22163feef136b476e
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2022-09-17 18:56:52 +0900

    fix top style

--- src/components/pages/Top.vue
+++ src/components/pages/Top.vue
@@ -1,8 +1,14 @@
 <template>
-  <img id="logo" alt="OnMeeP logo" src="../../assets/onmeep_logo.png">
-  <router-link to="/make-question">
-    <Btn btn-text="名刺テンプレートを作る"></Btn>
-  </router-link>
+  <div id="center">
+    <div id="topArea">
+      <img id="logo" alt="OnMeeP logo" src="../../assets/onmeep_logo.png">
+      <div id="btn">
+        <router-link to="/make-question">
+          <Btn btn-text="名刺テンプレートを作る"></Btn>
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -15,8 +21,26 @@ export default {
 </script>
 
 <style scoped lang="scss">
+#center {
+  width:100vw;
+  height: 100vh;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+}
+#topArea {
+  width: fit-content;
+  display: flex;
+  flex-flow: column;
+  gap: 10rem;
+  margin: 0 auto;
+}
 #logo {
-  width: 50%;
+  width: 70%;
+  margin: 0 auto;
+}
+#btn {
+  width: fit-content;
   margin: 0 auto;
 }
 </style>
