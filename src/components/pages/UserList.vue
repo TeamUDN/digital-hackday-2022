fd2513274dba68ed52cb38f919ebde9311432199
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2022-09-18 01:08:09 +0900

    add profile card sample

--- src/components/pages/UserList.vue
+++ src/components/pages/UserList.vue
@@ -8,6 +8,44 @@
         <p>Q2. 日頃の業務内容を教えて下さい</p>
         <p>Q3. 学生時代の部活は何をしていましたか？</p>
       </div>
+      <p class="title">プロフィールカード一覧</p>
+      <!-- TODO:DBから取得したデータをv-forで回してプロフィールカードを自動生成する -->
+      <!-- カード1 -->
+      <div class="whiteSquare">
+        <img class="profileIcon" alt="profile icon woman_1" src="../../assets/profile_icon/woman_1.png">
+        <div class="profileText">
+          <p class="cardName">しろくま</p>
+          <div class="userQuestion">
+            <p>A1. 他部署の社員との交流を深めたいと思ったから</p>
+            <p>A2. Webアプリのフロントエンドを中心に開発しています</p>
+            <p>A3. 吹奏楽部でバスクラリネットを担当していました</p>
+          </div>
+        </div>
+      </div>
+      <!-- カード2 -->
+      <div class="whiteSquare">
+        <img class="profileIcon" alt="profile icon woman_1" src="../../assets/profile_icon/woman_1.png">
+        <div class="profileText">
+          <p class="cardName">しろくま</p>
+          <div class="userQuestion">
+            <p>A1. 他部署の社員との交流を深めたいと思ったから</p>
+            <p>A2. Webアプリのフロントエンドを中心に開発しています</p>
+            <p>A3. 吹奏楽部でバスクラリネットを担当していました</p>
+          </div>
+        </div>
+      </div>
+      <!-- カード3 -->
+      <div class="whiteSquare">
+        <img class="profileIcon" alt="profile icon woman_1" src="../../assets/profile_icon/woman_1.png">
+        <div class="profileText">
+          <p class="cardName">しろくま</p>
+          <div class="userQuestion">
+            <p>A1. 他部署の社員との交流を深めたいと思ったから</p>
+            <p>A2. Webアプリのフロントエンドを中心に開発しています</p>
+            <p>A3. 吹奏楽部でバスクラリネットを担当していました</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -22,13 +60,39 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.center {
+  height: auto;
+}
+.mainArea {
+  margin: 5rem 0;
+}
 .title {
   font-size: 1.4rem;
   font-weight: bold;
   margin-bottom: -2.2rem;
 }
-.userQuestion {
+.whiteSquare {
+  width: fit-content;
   padding: 1.5rem 2rem;
+  display: flex;
+  gap: 2rem;
+  align-items: start;
+  img {
+    width: 6rem;
+    height: 6rem;
+    margin-top: 0.3rem;
+  }
+}
+.userQuestion {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: flex-start;
+}
+.cardName {
+  font-size: 1.5rem;
+}
+.profileText {
   display: flex;
   flex-direction: column;
   gap: 1rem;
