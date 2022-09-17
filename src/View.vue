93536a0c62cd61b78ec25d97a43b3cb9966884bd
Leon Kunishi <49759672+Kamenleon@users.noreply.github.com>
2022-09-18 03:22:12 +0900

    database_test
    database

--- src/View.vue
+++ src/View.vue
@@ -1,10 +1,38 @@
 <template>
-    <!-- $router.params.<router.jsで定義したパラメータ名>で参照できます。 -->
-    <div>画面{{ $route.params.id }}</div>
-  </template>
+  <!-- $router.params.<router.jsで定義したパラメータ名>で参照できます。 -->
+  <div>画面{{ $route.params.id }}</div>
+  <div id="add">
+    <Add></Add>
+  <Firestore></Firestore>
+  </div>
+</template>
   
   <script>
-    export default {
-      name: "View",
-    };
-  </script>
+import Firestore from './components/dataget.vue';
+import Add from './components/dataadd.vue';
+// import Add2 from './components/dataadd2.vue'
+// import Get2 from './components/dataget2.vue'
+
+export default {
+  // name: "View",
+  // components: {
+  //   Firestore
+  // },
+  name: 'add',
+  components: {
+    Add
+    ,Firestore
+  }
+};
+</script>
+
+<style>
+  #view {
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   text-align: center;
+   color: #2c3e50;
+   margin-top: 60px;
+  }
+  </style>
---
+++ src/components/dataget.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <h1>Firestore Test</h1>
+      <li v-for="(item, index) in data" :key="index">
+        {{ item }}
+      </li>
+    </div>
+   </template>
+
+<script>
+import db from "../firebase.js";
+
+export default {
+  name: "Firestore",
+  data() {
+    return {
+      data: [],
+    };
+  },
+  mounted: function () {
+    db.collection("OnMeeP")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          console.log(`${doc.id} => ${doc.data().name}`);
+          this.data.push(doc.data().name);
+        });
+      });
+  },
+};
+</script>
