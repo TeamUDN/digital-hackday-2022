6a3bf1b7dd11f54dd07c427d6334d9e2961a8d83
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2022-09-17 16:15:44 +0900

    add VueParticles

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,23 @@
 <template>
+  <vue-particles
+    color="#add8e6"
+    :particleOpacity="0.7"
+    linesColor="#add8e6"
+    :particlesNumber="80"
+    shapeType="circle"
+    :particleSize="6"
+    :linesWidth="2"
+    :lineLinked="true"
+    :lineOpacity="0.4"
+    :linesDistance="150"
+    :moveSpeed="4"
+    :hoverEffect="true"
+    hoverMode="grab"
+    :clickEffect="true"
+    clickMode="push"
+    retina_detect="true"
+  >
+  </vue-particles>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
@@ -25,5 +44,13 @@ export default {
   p{
     color: red;
   }
+}
+#particles-js {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -999;
 }
 </style>
