ecd86d25508bd2b057b9d072b7ffb0f351bc80cc
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2022-09-18 00:52:24 +0900

    add user-list page

---
+++ src/components/pages/UserList.vue
@@ -0,0 +1,37 @@
+<template>
+  <logoSmall/>
+  <div class="center">
+    <div class="mainArea">
+      <p class="title">参加者のみなさまへの質問</p>
+      <div class="whiteSquare userQuestion">
+        <p>Q1. 交流会に参加した理由は？</p>
+        <p>Q2. 日頃の業務内容を教えて下さい</p>
+        <p>Q3. 学生時代の部活は何をしていましたか？</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import logoSmall from '../design/RightTopLogo.vue'
+export default {
+  components: {
+    logoSmall,
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.title {
+  font-size: 1.4rem;
+  font-weight: bold;
+  margin-bottom: -2.2rem;
+}
+.userQuestion {
+  padding: 1.5rem 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: flex-start;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Top from './components/pages/Top.vue'
 import MakeQuestion from './components/pages/MakeQuestion.vue'
 import CreateUrl from './components/pages/CreateUrl.vue'
 import Question from './components/pages/Question.vue'
+import UserList from './components/pages/UserList.vue'
 import View from './View.vue';
 import NotFound from './components/NotFound.vue';
 
@@ -13,7 +14,8 @@ const router = createRouter({
     { path: '/', component:Top },
     { path: '/make-question', component: MakeQuestion },
     { path: '/create-url', component: CreateUrl },
-    { path: '/question/:id', component:Question },
+    { path: '/question/:id', component: Question },
+    { path: '/user-list/:id', component: UserList },
     { path: '/view/:id', component:View },
     { path: '/:notFound(.*)', component: NotFound }
   ],
