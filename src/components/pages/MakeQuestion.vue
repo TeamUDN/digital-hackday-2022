cee9ee603211dd342afb802575a1772e3b2ebd79
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2022-09-17 19:23:38 +0900

    add logo small design

--- src/components/pages/MakeQuestion.vue
+++ src/components/pages/MakeQuestion.vue
@@ -1,15 +1,18 @@
 <template>
-    <p>Make Question Page</p>
-    <router-link to="/make-question">
-      <Btn btn-text="URLを発行する"></Btn>
-    </router-link>
+  <logoSmall/>
+  <p>Make Question Page</p>
+  <router-link to="/make-question">
+    <Btn btn-text="URLを発行する"></Btn>
+  </router-link>
 </template>
 
 <script>
 import Btn from '../design/btn_design.vue'
+import logoSmall from '../design/RightTopLogo.vue'
 export default {
   components: {
     Btn,
+    logoSmall,
   },
 }
 </script>
