3297edde05e64d5fc4bf6e07c19b55bb1f67bb16
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2022-09-17 19:16:55 +0900

    add btn

--- src/components/pages/MakeQuestion.vue
+++ src/components/pages/MakeQuestion.vue
@@ -1,3 +1,18 @@
 <template>
     <p>Make Question Page</p>
+    <router-link to="/make-question">
+      <Btn btn-text="URLを発行する"></Btn>
+    </router-link>
 </template>
+
+<script>
+import Btn from '../design/btn_design.vue'
+export default {
+  components: {
+    Btn,
+  },
+}
+</script>
+
+<style scoped lang="scss">
+</style>
