52ef01800caf34a0add526121f16125f7ad75df9
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2022-09-18 11:34:24 +0900

    add top input

--- src/components/pages/Top.vue
+++ src/components/pages/Top.vue
@@ -6,6 +6,15 @@
         <router-link to="/make-question">
           <Btn btn-text="プロフィールカードのテンプレートを作成する"></Btn>
         </router-link>
+        <div class="passwordFlex">
+          <label for="urlParam"><p>パスワードを入力</p></label>
+          <input id="urlParam" name="urlParam" v-model="urlParam" placeholder="パスワードを入力" />
+        </div>
+        <div id="btn">
+        <button type="button" @click="loginURL()">
+          <Btn btn-text="招待された部屋に入室する"></Btn>
+        </button>
+      </div>
       </div>
     </div>
   </div>
@@ -17,6 +26,16 @@ export default {
   components: {
     Btn,
   },
+  data() {
+    return {
+      urlParam: '',
+    }
+  },
+  methods: {
+    loginURL() {
+      this.$router.push(`/question/${this.urlParam}`);
+    }
+  }
 }
 </script>
 
@@ -28,5 +47,15 @@ export default {
 #btn {
   width: fit-content;
   margin: 0 auto;
+}
+.passwordFlex {
+  width: fit-content;
+  display: flex;
+  gap: 1rem;
+  margin: 5rem auto 2rem;
+  align-items: center;
+  input {
+    padding: 0.5rem;
+  }
 }
 </style>
